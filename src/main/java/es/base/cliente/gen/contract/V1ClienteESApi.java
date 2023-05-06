baa302a325568c5cc263cc610fb01fe68dff6447
Miguel Angel reyes <mareyes46@misena.edu.co>
2023-05-06 10:51:44 -0500

    cambios ok

--- src/main/java/es/base/cliente/gen/contract/V1ClienteESApi.java
+++ src/main/java/es/base/cliente/gen/contract/V1ClienteESApi.java
@@ -2,18 +2,18 @@ package es.base.cliente.gen.contract;
 
 import es.base.cliente.gen.type.ClienteType;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 
 
 import java.io.InputStream;
 import java.util.Map;
 import java.util.List;
-import javax.validation.constraints.*;
-import javax.validation.Valid;
+import jakarta.validation.constraints.*;
+import jakarta.validation.Valid;
 
 @Path("/v1/es/cliente")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2023-05-06T10:18:05.076088800-05:00[America/Bogota]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2023-05-06T10:45:28.910391700-05:00[America/Bogota]")
 public interface V1ClienteESApi {
 
     @POST
